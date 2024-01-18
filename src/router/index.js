a077ff86355a17199864668b53f9b3381b2dac19
AbdulazizDevH <mr.abdulaziz00791@gmal.com>
2024-01-18 16:56:06 +0500

    Create home router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import homeLayout from '@/layouts/homeLayout.vue'
+import homePage from '@/views/home-page.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: "/",
+      name: "home-layout",
+      component: homeLayout,
+      children: [
+        {
+          path: "",
+          name: "home",
+          component: homePage
+        }
+      ]
+    }
     
   ]
 })

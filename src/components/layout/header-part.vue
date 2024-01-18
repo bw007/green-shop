7f734888c2d485ef3d345046f55870d890b6f571
AbdulazizDevH <mr.abdulaziz00791@gmal.com>
2024-01-18 16:56:31 +0500

    Header component

--- src/components/layout/header-part.vue
+++ src/components/layout/header-part.vue
@@ -1,7 +1,16 @@
 <template>
   <header class="header">
     <div class="container">
-
+      <router-link to="/" class="header__logo">
+        <img src="@/assets/imgs/header-logo.svg" alt="GREENSHOP">
+      </router-link>
+      <nav class="header__nav">
+        <router-link class="header__link" to="/">Home</router-link>
+        <router-link class="header__link" to="/shop">Shop</router-link>
+        <router-link class="header__link" to="/plant">Plant Care</router-link>
+        <router-link class="header__link" to="/blog">Blog</router-link>
+      </nav>
+      <div class="header__auth"></div>
     </div>
   </header>
 </template>
@@ -11,5 +20,22 @@
 </script>
 
 <style lang="scss">
-  
+  .header {
+    .container {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 25px 0;
+    }
+    &__logo {}
+    &__nav {}
+    &__link {
+      text-decoration: none;
+      color: #3D3D3D;
+      font-size: 1.6rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+  }
 </style>

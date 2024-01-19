cb24a026b0d13d15e56cf1229a9ea76e686d672c
bw007 <abdulaziz00791@gmail.com>
2024-01-19 07:45:13 +0500

    Change header

--- src/components/layout/header-part.vue
+++ src/components/layout/header-part.vue
@@ -1,16 +1,18 @@
 <template>
   <header class="header">
     <div class="container">
-      <router-link to="/" class="header__logo">
-        <img src="@/assets/imgs/header-logo.svg" alt="GREENSHOP">
-      </router-link>
-      <nav class="header__nav">
-        <router-link class="header__link" to="/">Home</router-link>
-        <router-link class="header__link" to="/shop">Shop</router-link>
-        <router-link class="header__link" to="/plant">Plant Care</router-link>
-        <router-link class="header__link" to="/blog">Blog</router-link>
-      </nav>
-      <div class="header__auth"></div>
+      <div class="header__inner">
+        <router-link to="/" class="header__logo">
+          <img src="@/assets/imgs/header-logo.svg" alt="GREENSHOP">
+        </router-link>
+        <nav class="header__nav">
+          <router-link class="header__link" to="/">Home</router-link>
+          <router-link class="header__link" to="/shop">Shop</router-link>
+          <router-link class="header__link" to="/plant">Plant Care</router-link>
+          <router-link class="header__link" to="/blog">Blog</router-link>
+        </nav>
+        <div class="header__auth"></div>
+      </div>
     </div>
   </header>
 </template>
@@ -20,22 +22,27 @@
 </script>
 
 <style lang="scss">
-  .header {
-    .container {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 25px 0;
-    }
-    &__logo {}
-    &__nav {}
-    &__link {
-      text-decoration: none;
-      color: #3D3D3D;
-      font-size: 1.6rem;
-      font-style: normal;
-      font-weight: 400;
-      line-height: normal;
-    }
+.header {
+  &__inner {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 20px;
+    padding-bottom: 18px;
+    border-bottom: 0.3px solid #46A35880;
   }
-</style>
+  &__logo {}
+  &__nav {
+    display: flex;
+    align-items: center;
+    gap: 50px;
+  }
+  &__link {
+    text-decoration: none;
+    color: #3D3D3D;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+}</style>

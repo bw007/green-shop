eb257a8520669cab6069e9e7ebfb49336ae9f098
bw007 <abdulaziz00791@gmail.com>
2024-01-20 09:54:35 +0500

    element ui css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import "./assets/css/main.css";
 import { createApp } from "vue";
 
 import ElementPlus from "element-plus";
+import 'element-plus/dist/index.css'
+
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 import { createPinia } from "pinia";

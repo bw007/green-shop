d85d1d021adca82d7420616e6c07545a5a6522f1
bw007 <abdulaziz00791@gmail.com>
2024-01-21 00:27:53 +0500

    404 page complete

--- src/views/error-page.vue
+++ src/views/error-page.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
-    404 NOT FOUND    
+    <el-row align="middle">
+      <el-text type="success">404</el-text>
+      <el-divider direction="vertical" />
+      Page not found
+    </el-row>
+    <router-link to="/">
+      <el-button type="warning" text>Back to home</el-button>
+    </router-link>
   </div>
 </template>
 
@@ -8,6 +15,21 @@
 
 </script>
 
-<style lang="scss">
-  
+<style lang="scss" scoped>
+.container {
+  height: calc(100vh - 120px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-size: 1.4rem;
+
+  .el-text {
+    font-size: 4rem;
+  }
+
+  .el-button {
+    margin-top: 20px;
+  }
+}
 </style>

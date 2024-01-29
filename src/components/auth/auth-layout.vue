a0b4449662ca1ecc757286f87c49b3a923496149
AbdulazizDevH <mr.abdulaziz00791@gmal.com>
2024-01-29 17:03:37 +0500

    Auth component

---
+++ src/components/auth/auth-layout.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-dialog
+    v-model="dialogToggle"
+    title="Tips"
+    width="500"
+    :before-close="handleClose"
+    :close-on-click-modal="false"
+    :close-on-press-escape="false"
+  >
+    <span>This is a message</span>
+
+    <el-radio-group v-model="radio" class="ml-4">
+      <el-radio label="1" size="large">Login</el-radio>
+      
+      <el-radio label="2" size="large">Register</el-radio>
+    </el-radio-group>
+
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="handleClose">Cancel</el-button>
+        <el-button type="primary" @click="handleClose">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+
+  </el-dialog>
+</template>
+
+<script setup>
+import { dialogStore } from "@/stores/utils/dialog";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+
+const radio = ref('1')
+
+console.log(radio.value);
+
+const dialog = dialogStore()
+const { dialogToggle } = storeToRefs(dialog)
+
+const handleClose = () => {
+  dialog.setDialogToggle(false)
+}
+
+</script>
+
+<style lang="">
+  
+</style>
---
+++ src/components/auth/login-part.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-dialog
+    v-model="dialogToggle"
+    title="Tips"
+    width="500"
+    :before-close="handleClose"
+    :close-on-click-modal="false"
+    :close-on-press-escape="false"
+  >
+    <span>This is a message</span>
+
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="Login" name="login">
+        
+        Login
+
+      </el-tab-pane>
+      <el-tab-pane label="Register" name="reg">
+        
+        Register
+
+      </el-tab-pane>
+    </el-tabs>
+
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="handleClose">Cancel</el-button>
+        <el-button type="primary" @click="handleClose">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+
+  </el-dialog>
+</template>
+
+<script setup>
+import { dialogStore } from "@/stores/utils/dialog";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+
+const activeName = ref('first')
+
+
+const dialog = dialogStore()
+const { dialogToggle } = storeToRefs(dialog)
+
+const handleClick = () => {
+  console.log(activeName.value);
+}
+
+const handleClose = () => {
+  dialog.setDialogToggle(false)
+}
+
+</script>
+
+<style lang="">
+  
+</style>

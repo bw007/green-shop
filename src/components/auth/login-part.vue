6b18da14f84fc7e5136cd3daa47bf4871ee7361a
bw007 <abdulaziz00791@gmail.com>
2024-01-31 23:16:58 +0500

    Auth complete

--- src/components/auth/login-part.vue
+++ src/components/auth/login-part.vue
@@ -1,34 +1,47 @@
 <template>
   <el-form 
     :model="form"
-    class="mt-50"
-    
   >
-    <p class="desc mb-14">Enter your username and password to login.</p>
+    <p class="auth__desc mb-14">Enter your username and password to login.</p>
     <el-form-item>
-      <el-input :input-style="{padding: '16px 4px' }" v-model="form.email" placeholder="Enter your email address" />
+      <el-input :input-style="{padding: '20px 4px' }" v-model="form.email" placeholder="Enter your email address" />
     </el-form-item>
     <el-form-item>
-      <el-input :input-style="{ padding: '16px 4px' }" v-model="form.password" placeholder="Password" show-password />
+      <el-input :input-style="{ padding: '20px 4px' }" v-model="form.password" placeholder="Password" show-password />
     </el-form-item>
     <el-form-item>
-      <el-link class="login-link" :underline="false" type="success">Forgot Password?</el-link>
+      <el-link class="auth__forgot" :underline="false" type="success">Forgot Password?</el-link>
     </el-form-item>
     <el-form-item>
-      <el-button :loading="loadingBtn" class="login-btn" type="success" @click="onSubmit">Login</el-button>
+      <el-button :loading="loading" class="auth__btn" type="success" @click="onSubmit(), loading = true">Login</el-button>
     </el-form-item>
-
-    
+    <div class="mt-45">
+      <el-divider class="auth__divider" content-position="center">Or login with</el-divider>
+    </div>
+    <el-form-item>
+      <div class="auth__btn-parent py-15">
+        <el-button class="auth__btn">
+          <google-icon />
+          <span class="auth__btn-text">Login with Google</span>
+        </el-button>
+      </div>
+      <div class="auth__btn-parent">
+        <el-button class="auth__btn">
+          <facebook-icon/>
+          <span class="auth__btn-text">Login with Facebook</span>
+        </el-button>
+      </div>
+    </el-form-item>
   </el-form>
 </template>
 
 <script setup>
-import { loadingStore } from "@/stores/utils/loading";
-import { storeToRefs } from "pinia";
 import { ref } from 'vue';
+import googleIcon from '@/assets/icons/google-icon.vue'
+import facebookIcon from '@/assets/icons/facebook-icon.vue'
 
-const loading = loadingStore()
-const { loadingBtn } = storeToRefs(loading)
+
+const loading = ref(false)
 
 const form = ref({
   email: "",
@@ -36,32 +49,6 @@ const form = ref({
 })
 
 const onSubmit = () => {
-  loading.setLoadingBtn(true)
-  console.log(100);
+  console.log(form.value);
 }
 </script>
-
-<style lang="scss">
-.el-form {}
-.el-input__inner {
-  
-}
-.login-link {
-  font-size: 1.4rem;
-  height: 16px;
-  margin-left: auto;
-}
-.login-btn {
-  width: 100%;
-  font-size: 1.6rem;
-  font-weight: 700;
-  padding: 16px;
-}
-
-.desc {
-  color: #3D3D3D;
-  font-size: 1.3rem;
-  line-height: 1.6rem;
-  text-align: center;
-}
-</style>
--- src/components/auth/register-part.vue
+++ src/components/auth/register-part.vue
@@ -1,37 +1,50 @@
 <template>
   <el-form 
     :model="form"
-    class="mt-50"
-    
   >
-    <p class="desc mb-14">Enter your username and password to login.</p>
+    <p class="auth__desc mb-14">Enter your email and password to register.</p>
     <el-form-item>
-      <el-input :input-style="{padding: '16px 4px' }" v-model="form.user" placeholder="Username" />
+      <el-input :input-style="{padding: '20px 4px' }" v-model="form.user" placeholder="Username" />
     </el-form-item>
     <el-form-item>
-      <el-input :input-style="{ padding: '16px 4px' }" v-model="form.email" placeholder="Enter your email address" />
+      <el-input :input-style="{ padding: '20px 4px' }" v-model="form.email" placeholder="Enter your email address" />
     </el-form-item>
     <el-form-item>
-      <el-input :input-style="{ padding: '16px 4px' }" v-model="form.password" placeholder="Password" show-password />
+      <el-input :input-style="{ padding: '20px 4px' }" v-model="form.password" placeholder="Password" show-password />
     </el-form-item>
     <el-form-item>
-      <el-input :input-style="{ padding: '16px 4px' }" v-model="form.confirmPswd" placeholder="Confirm Password" show-password />
+      <el-input :input-style="{ padding: '20px 4px' }" v-model="form.confirmPswd" placeholder="Confirm Password" show-password />
     </el-form-item>
     <el-form-item>
-      <el-button :loading="loadingBtn" class="login-btn" type="success" @click="onSubmit">Register</el-button>
+      <el-button :loading="loading" class="auth__btn" type="success" @click="onSubmit(), loading = true">Register</el-button>
     </el-form-item>
-
+    <div class="mt-45">
+      <el-divider class="auth__divider" content-position="center">Or register with</el-divider>
+    </div>
+    <el-form-item>
+      <div class="auth__btn-parent py-15">
+        <el-button class="auth__btn">
+          <google-icon />
+          <span class="auth__btn-text">Continue with Google</span>
+        </el-button>
+      </div>
+      <div class="auth__btn-parent">
+        <el-button class="auth__btn">
+          <facebook-icon/>
+          <span class="auth__btn-text">Continue with Facebook</span>
+        </el-button>
+      </div>
+    </el-form-item>
     
   </el-form>
 </template>
 
 <script setup>
-import { loadingStore } from "@/stores/utils/loading";
-import { storeToRefs } from "pinia";
 import { ref } from 'vue';
+import googleIcon from '@/assets/icons/google-icon.vue'
+import facebookIcon from '@/assets/icons/facebook-icon.vue'
 
-const loading = loadingStore()
-const { loadingBtn } = storeToRefs(loading)
+const loading = ref(false)
 
 const form = ref({
   user: "",
@@ -41,11 +54,6 @@ const form = ref({
 })
 
 const onSubmit = () => {
-  loading.setLoadingBtn(true)
-  console.log(100);
+  console.log(form.value);
 }
 </script>
-
-<style lang="">
-  
-</style>

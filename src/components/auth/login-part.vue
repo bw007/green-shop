e15daf325e3d9d9aa9e50174e890b2a578fbf221
bw007 <abdulaziz00791@gmail.com>
2024-02-01 00:15:41 +0500

    Form validation

--- src/components/auth/login-part.vue
+++ src/components/auth/login-part.vue
@@ -1,19 +1,21 @@
 <template>
   <el-form 
     :model="form"
+    ref="ruleFormRef"
+    :rules="rules"
   >
     <p class="auth__desc mb-14">Enter your username and password to login.</p>
-    <el-form-item>
+    <el-form-item prop="email" class="pb-5">
       <el-input :input-style="{padding: '20px 4px' }" v-model="form.email" placeholder="Enter your email address" />
     </el-form-item>
-    <el-form-item>
+    <el-form-item prop="password">
       <el-input :input-style="{ padding: '20px 4px' }" v-model="form.password" placeholder="Password" show-password />
     </el-form-item>
     <el-form-item>
       <el-link class="auth__forgot" :underline="false" type="success">Forgot Password?</el-link>
     </el-form-item>
     <el-form-item>
-      <el-button :loading="loading" class="auth__btn" type="success" @click="onSubmit(), loading = true">Login</el-button>
+      <el-button :loading="loading" class="auth__btn" type="success" @click="onSubmit(ruleFormRef), loading = true">Login</el-button>
     </el-form-item>
     <div class="mt-45">
       <el-divider class="auth__divider" content-position="center">Or login with</el-divider>
@@ -40,15 +42,51 @@ import { ref } from 'vue';
 import googleIcon from '@/assets/icons/google-icon.vue'
 import facebookIcon from '@/assets/icons/facebook-icon.vue'
 
-
-const loading = ref(false)
+const ruleFormRef = ref()
 
 const form = ref({
   email: "",
   password: ""
 })
 
-const onSubmit = () => {
-  console.log(form.value);
+const rules = ref({
+  email: [
+    {
+      required: true,
+      message: "Please input email address",
+      trigger: "blur"
+    },
+    {
+      type: 'email',
+      message: 'Please input correct email address',
+      trigger: ['blur', 'change']
+    }
+  ],
+  password: [
+    { 
+      required: true, 
+      message: 'Please input password', 
+      trigger: 'blur' 
+    },
+    { 
+      min: 3,
+      message: 'Please length greater than 3', 
+      trigger: ['blur', 'change'] 
+    },
+  ]
+})
+
+const loading = ref(false)
+
+const onSubmit = async (formEl) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log(valid, fields);
+    } else {
+      console.log('error submit!', fields)
+      loading.value = false
+    }
+  })
 }
 </script>
--- src/components/auth/register-part.vue
+++ src/components/auth/register-part.vue
@@ -1,22 +1,24 @@
 <template>
   <el-form 
     :model="form"
+    ref="ruleFormRef"
+    :rules="rules"
   >
     <p class="auth__desc mb-14">Enter your email and password to register.</p>
-    <el-form-item>
+    <el-form-item prop="" class="pb-5">
       <el-input :input-style="{padding: '20px 4px' }" v-model="form.user" placeholder="Username" />
     </el-form-item>
-    <el-form-item>
+    <el-form-item prop="" class="pb-5">
       <el-input :input-style="{ padding: '20px 4px' }" v-model="form.email" placeholder="Enter your email address" />
     </el-form-item>
-    <el-form-item>
+    <el-form-item prop="password" class="pb-5">
       <el-input :input-style="{ padding: '20px 4px' }" v-model="form.password" placeholder="Password" show-password />
     </el-form-item>
-    <el-form-item>
+    <el-form-item prop="confirmPswd" class="pb-5">
       <el-input :input-style="{ padding: '20px 4px' }" v-model="form.confirmPswd" placeholder="Confirm Password" show-password />
     </el-form-item>
     <el-form-item>
-      <el-button :loading="loading" class="auth__btn" type="success" @click="onSubmit(), loading = true">Register</el-button>
+      <el-button :loading="loading" class="auth__btn" type="success" @click="onSubmit(ruleFormRef), loading = true">Register</el-button>
     </el-form-item>
     <div class="mt-45">
       <el-divider class="auth__divider" content-position="center">Or register with</el-divider>
@@ -46,6 +48,8 @@ import facebookIcon from '@/assets/icons/facebook-icon.vue'
 
 const loading = ref(false)
 
+const ruleFormRef = ref()
+
 const form = ref({
   user: "",
   email: "",
@@ -53,7 +57,66 @@ const form = ref({
   confirmPswd: ""
 })
 
-const onSubmit = () => {
-  console.log(form.value);
+const rules = ref({
+  user: [
+    { 
+      required: true, 
+      message: 'Please input username', 
+      trigger: 'blur' 
+    },
+    { 
+      min: 3,
+      message: 'Please length greater than 3', 
+      trigger: ['blur', 'change'] 
+    },
+  ],
+  email: [
+    {
+      required: true,
+      message: "Please input email address",
+      trigger: "blur"
+    },
+    {
+      type: 'email',
+      message: 'Please input correct email address',
+      trigger: ['blur', 'change']
+    }
+  ],
+  password: [
+    { 
+      required: true, 
+      message: 'Please input password', 
+      trigger: 'blur' 
+    },
+    { 
+      min: 3,
+      message: 'Please length greater than 3', 
+      trigger: ['blur', 'change'] 
+    },
+  ],
+  confirmPswd: [
+    { 
+      required: !form.value.password, 
+      message: 'Please input password', 
+      trigger: 'blur' 
+    },
+    { 
+      min: 3,
+      message: 'Please length greater than 3', 
+      trigger: ['blur', 'change'] 
+    },
+  ]
+})
+
+const onSubmit = async (formEl) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log(fields);
+    } else {
+      console.log('error submit!', fields)
+      loading.value = false
+    }
+  })
 }
 </script>
